362ff1084b45f8c54fc88592742db08d99a40cc5
rozsypalovaz <zuzana.rozsypalova@tul.cz>
2025-01-12 17:31:16 +0100

    Prokliky z loginu. signupu na main page 2

--- app/src/main/java/com/example/semestralniprojektpma/SignupActivity.java
+++ app/src/main/java/com/example/semestralniprojektpma/SignupActivity.java
@@ -1,5 +1,7 @@
 package com.example.semestralniprojektpma;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,15 +10,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.semestralniprojektpma.HelperClass;
+import com.example.semestralniprojektpma.LoginActivity;
+import com.example.semestralniprojektpma.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
 
     EditText signupName, signupUsername, signupEmail, signupPassword;
-    TextView loginRedirectText, bezPrihlaseni;  // Přidání TextView bezPrihlaseni
+    TextView loginRedirectText;
     Button signupButton;
     FirebaseDatabase database;
     DatabaseReference reference;
@@ -32,11 +35,11 @@ public class SignupActivity extends AppCompatActivity {
         signupPassword = findViewById(R.id.signup_password);
         loginRedirectText = findViewById(R.id.loginRedirectText);
         signupButton = findViewById(R.id.signup_button);
-        bezPrihlaseni = findViewById(R.id.bezPrihlaseni);  // Inicializace TextView
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 database = FirebaseDatabase.getInstance();
                 reference = database.getReference("users");
 
@@ -48,7 +51,7 @@ public class SignupActivity extends AppCompatActivity {
                 HelperClass helperClass = new HelperClass(name, email, username, password);
                 reference.child(username).setValue(helperClass);
 
-                Toast.makeText(SignupActivity.this, "You have signed up successfully!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignupActivity.this, "You have signup successfully!", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
@@ -58,14 +61,6 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        bezPrihlaseni.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, MainActivity.class);  // Přesměrování na MainActivity
                 startActivity(intent);
             }
         });
